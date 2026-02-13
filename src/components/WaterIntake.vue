e0cca3f0e5173ad9d6e3bbb9673a715ac39d512e
Viviana <40200339@esmad.ipp.pt>
2026-02-13 15:59:41 +0000

    added and slightly altered water animation from codePen

--- src/components/WaterIntake.vue
+++ src/components/WaterIntake.vue
@@ -1,8 +1,10 @@
 <template>
-  <section class="component">
-    <h2>WATER</h2>
-    <p>goal: 2000ml</p>
-    <p>achieved: 500ml</p>
+  <section class="component water">
+    <div>
+      <h2>WATER</h2>
+      <p>goal: 2000ml</p>
+      <p>achieved: 500ml</p>
+    </div>
   </section>
 </template>
 
@@ -10,11 +12,39 @@
 
 <style scoped>
 section {
+  position: relative;
+  overflow: hidden;
+
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 
   background-color: #77b9ff;
+}
+
+div {
+  z-index: 1;
+}
+
+.water::before {
+  content: "";
+  width: 200%;
+  height: 200%;
+  background-color: #8ac3ff;
+  position: absolute;
+  top: -150%;
+  left: -50%;
+  border-radius: 40%;
+  animation: anim 12s linear infinite;
+}
+
+@keyframes anim {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
